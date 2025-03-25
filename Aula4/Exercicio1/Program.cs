23f39bbcfba4147b50152f671a837b6e7b80e1df
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-03-24 22:38:10 -0300

    Add files via upload

---
+++ Aula4/Exercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public abstract class Pagamento
+{
+    public virtual void ProcessarPagamento(){
+        Console.WriteLine("Processando pagamento");
+    }
+}
+public class CartaoCredito : Pagamento{
+    public override void ProcessarPagamento(){
+        Console.WriteLine("Pagamento realizado por Cartao de Credito");
+    }
+}
+public class BoletoBancario : Pagamento{
+    public override void ProcessarPagamento(){
+        Console.WriteLine("Pagamento realizado por Boleto Bancario");
+    }
+}
+public class Pix : Pagamento{
+    public override void ProcessarPagamento(){
+        Console.WriteLine("Pagamento realizado por Pix");
+    }
+}
+class Program
+{
+static void Main()
+{
+    Pagamento[] pagamentos = new Pagamento[3];
+
+    pagamentos[0] = new CartaoCredito();
+    pagamentos[1] = new BoletoBancario();
+    pagamentos[2] = new Pix();
+
+    foreach (var pagamento in pagamentos){
+        pagamento.ProcessarPagamento();
+    }
+
+}
+}
---
+++ Aula4/Exercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public abstract class Funcionario
+{
+    public abstract decimal CalcularSalario();
+}
+public class Gerente : Funcionario {
+    private decimal salario;
+    public Gerente (decimal salario){
+        this.salario = salario;
+    }
+    public override decimal CalcularSalario()
+    {
+        return salario + (salario * 0.7m);
+    }
+
+}
+public class Programador : Funcionario {
+    private decimal salario;
+    public Programador (decimal salario){
+        this.salario = salario;
+    }
+    public override decimal CalcularSalario()
+    {
+        return salario + (salario * 0.3m);
+    }
+
+}
+
+class Program
+{
+static void Main()
+{
+    Funcionario gerente = new Gerente (10000m);
+    Funcionario programador = new Programador (4000m);
+
+    Console.WriteLine ("Salario do Gerente" + gerente.CalcularSalario());
+    Console.WriteLine ("Salario do Programador" + programador.CalcularSalario());
+
+}
+}
