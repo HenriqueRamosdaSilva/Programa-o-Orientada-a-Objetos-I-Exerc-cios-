b0048d7e156f91c7204a45359fff348eee1cb8a4
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-03-25 21:11:26 -0300

    Add files via upload

---
+++ Aula2/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,55 @@
+﻿
+﻿using System;
+
+class Elevador{
+    private int _andarAtual;
+    private int totalAndares;
+    
+    public Elevador( int totalAndares){
+        this._andarAtual = 0;
+        this.totalAndares = totalAndares;
+    }
+    public int AndarAtual {
+        get { return _andarAtual; } // Retorna o saldo
+        set{
+            if (value >= 0 && value <= totalAndares)
+                _andarAtual = value;
+            else
+                Console.WriteLine("Andar Inválido");
+        }
+    }
+    public void Subir(){
+        if(_andarAtual < totalAndares)
+        {
+            _andarAtual++;
+        }
+        else{
+            Console.WriteLine("Ultimo Andar");
+        }
+    }
+    public void Descer(){
+        if(_andarAtual > 0)
+        {
+           _andarAtual--;
+        }
+        else{
+            Console.WriteLine("Primeiro Andar");
+        }
+    }
+
+}
+
+class Program{
+static void Main()
+{
+    Elevador e = new Elevador(10); // 10 andares
+    e.Subir();
+    e.Subir();
+    Console.WriteLine(e.AndarAtual); // Deve exibir 2
+    e.Descer();
+    Console.WriteLine(e.AndarAtual); // Deve exibir 1
+    e.Descer();
+    e.Descer(); // Deve continuar em 0
+
+}
+}
