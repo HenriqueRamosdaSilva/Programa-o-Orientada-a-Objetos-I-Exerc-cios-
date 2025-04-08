34ec9c9f23d7ec87a976cc1871a253bd29fac765
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-04-07 22:34:03 -0300

    Add files via upload

---
+++ Aula5/Exercicio1/Exercicio 01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program{
+    static void Main()
+    {
+        for(int i = 1 ; i<=5; i++){
+        Console.WriteLine("Digite o " + i +"decimal: ");
+        double numero =double.Parse (Console.ReadLine());
+        Console.WriteLine("O número original:"+ numero);
+        double aredondamento = Math.Round(numero);
+        Console.WriteLine("O valor com arredondamento padrão: "+ aredondamento);
+        double aredondamento_baixo = Math.Floor(numero);
+        Console.WriteLine("O valor com arredondamento para baixo: "+ aredondamento_baixo);
+        double aredondamento_cima = Math.Ceiling(numero);
+        Console.WriteLine("O valor com arredondamento para cima: "+ aredondamento_cima);
+        double aredondamento_truncado = Math.Truncate(numero);
+        Console.WriteLine("O valor com truncamento: "+ aredondamento_truncado);
+        }
+    }
+}
---
+++ Aula5/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase com espaços no início e no fim, e com palavras separadas por vírgulas ");
+        string frase = Console.ReadLine();
+        string fraseTrim = frase.Trim();
+        Console.WriteLine("Sem os espaços extras da frase:"+ fraseTrim );
+        string [] fraseSplit =frase.Split(','); 
+        Console.WriteLine("Frase dividida em partes: ");
+        foreach (string palavra in fraseSplit){
+            Console.WriteLine(palavra.Trim());
+        }
+        Console.WriteLine("Digite a palavra que você deseja substituir:");
+        string palavraAntiga = Console.ReadLine();
+        Console.WriteLine("Digite a palavra nova para substituir:");
+        string palavraNova = Console.ReadLine();
+
+        frase = frase.Replace(palavraAntiga, palavraNova);
+        Console.WriteLine("Frase após substituição: " + frase);
+
+        Console.WriteLine("Digite a palavra que deseja verificar, se a frase começa com determinada palavra: " );
+        string palavraComeca = Console.ReadLine().Trim();
+
+        if (frase.Trim().StartsWith(palavraComeca))
+        {
+            Console.WriteLine("A frase começa com a palavra '" + palavraComeca);
+        }
+        else
+        {
+            Console.WriteLine("A frase NÃO começa com a palavra '" + palavraComeca);
+        }
+        Console.WriteLine("Digite a palavra que deseja verificar, se a frase termina com determinada palavra: " );
+        string palavraTermina = Console.ReadLine().Trim();
+         if (frase.Trim().EndsWith(palavraTermina))
+        {
+            Console.WriteLine("A frase termina com a palavra '" + palavraTermina);
+        }
+        else
+        {
+            Console.WriteLine("A frase NÃO termina com a palavra '" + palavraTermina);
+        }
+    }
+}
